2f32e8640b6bf653a76d973eec450490405b4de6
mainitiamai <mainitiamai@gmail.com>
2015-03-27 00:00:46 +0900

    NetworkViewのテストプログラム 思ったよりカクカクする

---
+++ Assets/GameMain.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using gui = UnityEngine.GUI;
+
+public class GameMain : MonoBehaviour {
+
+	public GameObject playerPrefab;
+	string ip = "127.0.0.1";
+	string port = "4211";
+	Camera camera;
+
+	bool connected;
+
+	public void CreatePlayer(){
+		connected = true;
+		var g = (GameObject)Network.Instantiate (playerPrefab, transform.position, transform.rotation, 1);
+		g.GetComponentInChildren<Camera> ().enabled = true;
+		camera.enabled = false;
+	}
+
+	void OnDisconnectedFromServer(){
+		connected = false;
+	}
+
+	void OnPlayerDisconnected(NetworkPlayer pl){
+		Network.DestroyPlayerObjects (pl);
+	}
+
+	void OnConnectedToServer(){
+		CreatePlayer ();
+	}
+
+	void OnServerInitialized(){
+		CreatePlayer ();
+	}
+
+	void OnGUI(){
+		if (!connected) {
+			ip = gui.TextField (new Rect (10, 10, 90, 20), ip);
+			port = gui.TextField( new Rect( 10,40,90,20),port);
+		}
+		if (gui.Button (new Rect (10, 70, 90, 20), "connect")) {
+			Network.Connect (ip, int.Parse (port));
+		}
+		if (gui.Button (new Rect (10, 100, 90, 20), "host")) {
+			Network.InitializeServer (10, int.Parse (port), false);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		camera = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(NetworkView))]
+public class Player : MonoBehaviour {
+
+	private Transform tf;
+	private Rigidbody rb;
+	private Camera camera;
+
+	private NetworkView nv;
+	// Use this for initialization
+	void Start () {
+		tf = transform;
+		rb = GetComponent<Rigidbody> ();
+		nv = GetComponent<NetworkView> ();
+		camera = GetComponent<Camera> ();
+		return;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (nv.isMine) {
+			if (Input.GetButton ("Vertical")) {
+				tf.position += tf.forward * 0.5f;
+			}
+			if (Input.GetButton ("Horizontal")) {
+				tf.RotateAround (tf.TransformDirection (new Vector3 (0.0f, 1.0f, 0.0f)), 0.1f * Input.GetAxisRaw ("Horizontal"));
+			}
+		}
+	}
+}
